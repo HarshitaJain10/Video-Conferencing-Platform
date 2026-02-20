52319085c6398d1f5ee367c67aeda58cf8fab10a
Harshita Jain <khushijain10sep1993@gmail.com>
2026-02-20 06:05:13 +0530

    Deploying Frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,7 @@
-const server = "http://localhost:8000";
+let IS_PROD=true;
+const server =  IS_PROD?
+    
+   "https://video-conferencing-platform-backend-8vje.onrender.com" :
+    "http://localhost:8000"
+
 export default server;
